4e41cd2cdaf26f58787ee90320593aa7f885395e
Osama Ahmed <osama.des795@gmail.com>
2025-04-01 16:05:03 +0200

    Fix: Wrap useSearchParams with use client and force-dynamic

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import QuestionsList from "./_components/QuestionsList";
 
+export const dynamic = "force-dynamic"; // ⬅️ إجبار الصفحة على أن تكون ديناميكية بالكامل
+
 export default function Home() {
   return (
       <QuestionsList />
