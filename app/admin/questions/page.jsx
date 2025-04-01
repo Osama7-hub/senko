3ac0cf042a8715aaca0a64977223dd94f40f5b17
Osama Ahmed <osama.des795@gmail.com>
2025-04-01 15:52:15 +0200

    Fix: Wrap useSearchParams with force-dynamic

--- app/admin/questions/page.jsx
+++ app/admin/questions/page.jsx
@@ -3,6 +3,8 @@
 import MainLayout from "@/app/layouts/MainLayout";
 import QuestionsClient from "./QuestionsClient.js";
 
+export const dynamic = "force-dynamic"; // ⬅️ هذا السطر يمنع Next.js من معالجة الصفحة أثناء الـ build
+
 export default function QuestionsPage() {
     return (
         <MainLayout header={<span className="mb-4 font-bold text-2xl">إدارة الأسئلة</span>}>
