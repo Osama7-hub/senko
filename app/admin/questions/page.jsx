310ee4abd672f3489802b4d9fbe818eaa55757b3
Osama Ahmed <osama.des795@gmail.com>
2025-04-01 16:32:06 +0200

    Fix: Wrap useSearchParams in Suspense Boundary

--- app/admin/questions/page.jsx
+++ app/admin/questions/page.jsx
@@ -2,6 +2,7 @@
 
 import MainLayout from "@/app/layouts/MainLayout";
 import QuestionsClient from "./QuestionsClient.js";
+import { Suspense } from "react";
 
 export const dynamic = "force-dynamic"; // ⬅️ هذا السطر يمنع Next.js من معالجة الصفحة أثناء الـ build
 
