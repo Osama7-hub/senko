9f6d07e04355af7198dbd4396290730ac0f6ecf4
Osama Ahmed <osama.des795@gmail.com>
2025-04-01 16:07:54 +0200

    Fix: Wrap useSearchParams in Suspense Boundary

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
 "use client";
+import { Suspense } from "react";
 import QuestionsList from "./_components/QuestionsList";
 
 export const dynamic = "force-dynamic"; // ⬅️ إجبار الصفحة على أن تكون ديناميكية بالكامل
 
 export default function Home() {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
       <QuestionsList />
+    </Suspense>
   );
 }
